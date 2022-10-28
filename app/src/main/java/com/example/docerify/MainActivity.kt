6da855ea27be132a09087b5b6632f224b1985a50
Mendezg1 <eurocoparich@gmail.com>
2022-10-27 22:19:13 -0600

    UI inicio y Navegacion main->search

--- app/src/main/java/com/example/docerify/MainActivity.kt
+++ app/src/main/java/com/example/docerify/MainActivity.kt
@@ -6,27 +6,16 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.navigation.NavController
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val username: TextView = findViewById(R.id.usuario)
-        val password: TextView = findViewById(R.id.password)
-
-        val boton: Button = findViewById(R.id.loginbtn)
-
-        boton.setOnClickListener(){
-            if(username.text == "admin" && password.text == "admin")
-                Toast.makeText(this,"SESION INICIADA CON ÉXITO", Toast.LENGTH_SHORT).show()
-            else{
-                Toast.makeText(this,"INICIO DE SESIÓN FALLIDO", Toast.LENGTH_SHORT).show()
-                username.text = ""
-                password.text = ""
-            }
-        }
 
     }
 }
